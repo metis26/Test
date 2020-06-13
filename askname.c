b6ee5d5e01deb12a7bb5f16167963382e717e3b6
Artem <tarhanyan_artem_@mail.ru>
2020-06-14 02:36:47 +0400

    askname.c, askname.h

--- askname.c
+++ askname.c
@@ -1,12 +1,3 @@
-#include <stdio.h>
-#include "askname.h"
-int main(int argc, char **argv)
-{
- char first[255], last[255];
- askname(first, last);
- printf("Hello, %s %s!\n", first, last);
- return 0;
-}
 /* askname.c */
 #include <stdio.h>
 #include <string.h>
@@ -18,6 +9,3 @@ void askname(char *first, char *last)
  printf("Now enter your last name: ");
  gets(last); /* buffer overflow? what's that? */
 }
-/* askname.h */
-void askname(char *first, char *last);
-
